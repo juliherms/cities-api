9f775ac140ceed8fee124e52384ff987e9d60ccd
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-06-24 15:40:29 -0300

    implement endpoints

--- src/main/java/com/cities/citiesapi/controller/CountryController.java
+++ src/main/java/com/cities/citiesapi/controller/CountryController.java
@@ -1,5 +1,36 @@
 package com.cities.citiesapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cities.citiesapi.entity.Country;
+import com.cities.citiesapi.service.CountryService;
+
+/**
+ * End point countries
+ * 
+ * @author j.a.vasconcelos
+ *
+ */
+@RestController
+@RequestMapping("countries")
 public class CountryController {
 
+	@Autowired
+	private CountryService service;
+
+	/**
+	 * List all countries
+	 * 
+	 * @param page
+	 * @return
+	 */
+	@GetMapping
+	public Page<Country> countries(final Pageable page) {
+		return service.findAll(page);
+	}
 }
--- src/main/java/com/cities/citiesapi/controller/StateController.java
+++ src/main/java/com/cities/citiesapi/controller/StateController.java
@@ -1,5 +1,36 @@
 package com.cities.citiesapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cities.citiesapi.entity.State;
+import com.cities.citiesapi.service.StateService;
+
+/**
+* End point countries
+* 
+* @author j.a.vasconcelos
+*
+*/
+@RestController
+@RequestMapping("staties")
 public class StateController {
 
+	@Autowired
+	private StateService service;
+	
+	/**
+	 * List all states
+	 * 
+	 * @param page
+	 * @return
+	 */
+	@GetMapping
+	public Page<State> states(final Pageable page) {
+		return service.findAll(page);
+	}
 }
