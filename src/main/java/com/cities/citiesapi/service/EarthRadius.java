11b6819f6bfcb9c8fdabffea44a709f784fbc0b9
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-06-24 21:22:41 -0300

    add swagger

--- src/main/java/com/cities/citiesapi/service/EarthRadius.java
+++ src/main/java/com/cities/citiesapi/service/EarthRadius.java
@@ -1,6 +1,16 @@
 package com.cities.citiesapi.service;
 
+import io.swagger.annotations.ApiModel;
+
+/**
+ * Enum represents unit for radius
+ * 
+ * @author j.a.vasconcelos
+ *
+ */
+@ApiModel(value = "EarthRadius", description = "Unidade de medida de retorno da distância")
 public enum EarthRadius {
+
 	METERS("m", 6378168), KILOMETERS("km", 6378.168f), MILES("mi", 3958.747716f);
 
 	private final String unit;
